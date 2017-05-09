93ca95d57a4783a9e409bc406eb1bb7be5fb0af3
Gonzalo <gdelacruzfdez@gmail.com>
2017-05-10 00:21:37 +0200

    ARREGLADO

--- participationSystem_i3b/src/main/java/hello/services/UserDataService.java
+++ participationSystem_i3b/src/main/java/hello/services/UserDataService.java
@@ -1,11 +1,16 @@
 package hello.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import hello.model.CitizenDTO;
 import hello.model.User;
 import hello.repository.DBServiceImpl;
 
@@ -19,15 +24,23 @@ public class UserDataService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
 		User user = dbService.getUser(username);
-		if(user!=null){
-			return user;
+		if (user != null) {
+			RestTemplate restTemplate = new RestTemplate();
+			Map<String, String> vars = new HashMap<String, String>();
+			vars.put("login", user.getEmail());
+			vars.put("password", user.getPassword());
+			CitizenDTO userDTO = restTemplate.postForObject(
+					"http://localhost:8091/user", vars, CitizenDTO.class);
+
+			if (userDTO != null) {
+				return user;
+			}
 		}
-		 throw new UsernameNotFoundException("Username not found");
-	}
-	
-	public void addUserData(User user){
-		dbService.addUser(user);
+		throw new UsernameNotFoundException("Username not found");
 	}
 
+	public void addUserData(User user) {
+		dbService.addUser(user);
+	}
 
 }
