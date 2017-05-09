6a040cf075d3f010121de049af2cf5bc053f9010
Gonzalo <gdelacruzfdez@gmail.com>
2017-05-10 00:32:46 +0200

    Arreglados tests

--- participationSystem_i3b/src/test/java/hello/ModelTests.java
+++ participationSystem_i3b/src/test/java/hello/ModelTests.java
@@ -16,11 +16,11 @@ public class ModelTests {
 		assertEquals("testId", c1.getId());
 		c1.setIdProposal("propId");
 		c1.setUser(new User());
-		c1.getUser().setName("tuser");
+		c1.getUser().setEmail("tuser");
 		assertTrue(c1.toString().contains("tuser"));
 		assertTrue(c1.toString().contains("testId"));
 	}
-	
+
 	@Test
 	public void testProposal() {
 		Proposal p1 = new Proposal(null, "cat1", "title", "content");
@@ -29,19 +29,18 @@ public class ModelTests {
 		p1.setAccepted(true);
 		assertTrue(p1.isAccepted());
 		User a = new User();
-		a.setName("tuser");
+		a.setEmail("tuser");
 		p1.setAuthor(a);
 		assertTrue(p1.getAuthor().equals(a));
 		assertTrue(p1.toString().contains("tuser"));
 		assertTrue(p1.toString().contains("cat1"));
 	}
-	
+
 	@Test
 	public void testUser() {
 		User c1 = new User();
 		c1.setAdmin(true);
-		assertNull(c1.getAge());
-		assertNull(c1.getId());
+
 	}
 
 }
